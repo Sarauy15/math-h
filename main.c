af74a85c838e8f8fa3cfa24616ce0f0e1f0b2ab1
Sarauy15 <127320112+Sarauy15@users.noreply.github.com>
2025-01-27 23:15:27 +0100

    Add files via upload
    esempio

---
+++ main.c
@@ -0,0 +1,13 @@
+//Write a complete program that prompts the user to enter three integers and calculates the product.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main(){
+    int n1,n2,n3,p;
+    printf("dammi i tre numeri interi: \n");
+    scanf("%d%d%d",&n1,&n2,&n3);
+    printf("il prodotto dei tre numeri viene: %d ", p=n1*n2*n3);
+
+ return 0;
+ }
